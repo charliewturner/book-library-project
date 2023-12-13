f204f0824e702ef792744c931d172d285254d5fc
Charlie Turner <c.w.turner@hotmail.com>
2023-12-13 18:18:40 +0000

    Alter JS to include form query selectors

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const myLibrary = [];
 
-const author = document.getElementById("author");
-const title = document.getElementById("title");
-const pageCount = document.getElementById("pageCount");
-const readStatus = document.getElementById("readStatus");
+$form = document.querySelector(".form");
+$titleInput = form.querySelector("#titleInput");
+$authorInput = form.querySelector("#authorInput");
+$pageCountInput = form.querySelector("#pageCountInput");
+$readStatusInput = form.querySelector("#readStatusInput");
+$submit = form.querySelector("#submit");
 
 function Book(author, title, pageCount, readStatus) {
   // the constructor...
-    this.author = author;
-    this.title = title;
-    this.pageCount = pageCount;
-    this.readStatus = readStatus;
+  this.author = author;
+  this.title = title;
+  this.pageCount = pageCount;
+  this.readStatus = readStatus;
     
-    this.shareDetails = function () {
-        console.log(this.author, this.title, this.pageCount, this.readStatus);
-    }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary = () => {
   // do stuff here
   console.log("button enabled");
+
 }
 
 const submit = document.getElementById("submit");
