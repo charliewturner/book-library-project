211f7ef5e42b960129d215975668bd21d93dc2b0
Charlie Turner <c.w.turner@hotmail.com>
2024-03-21 17:51:56 +0000

    Refactor constructor into class

--- script.js
+++ script.js
@@ -15,13 +15,11 @@ const myLibrary = [
 
 $submit = document.querySelector("#submit");
 
-function Book(title, author, pageCount, readStatus) {
-  // the constructor...
-  this.author = author;
-  this.title = title;
-  this.pageCount = pageCount;
-  this.readStatus = readStatus;
-}
+$submit.addEventListener('click', () => {
+  event.preventDefault();
+  addBookToLibrary();
+  displayLibrary();
+})
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
@@ -30,74 +28,156 @@ const closeButton = document.querySelector("dialog button");
 showButton.addEventListener("click", () => {
   dialog.showModal();
 });
+
 closeButton.addEventListener("click", () => {
   dialog.close();
 });
 
-const addBookToLibrary = () => {
-  // do stuff here
-  let title = document.querySelector("#titleInput").value;
-  let author = document.querySelector("#authorInput").value;
-  let pageCount = document.querySelector("#pageCountInput").value;
-  let readStatus = checkReadStatus();
-  let newBook = new Book(title, author, pageCount, readStatus);
-  myLibrary.push(newBook);
-  console.log(myLibrary);
 
-}
-
-function checkReadStatus() {
-  if (document.querySelector("#readStatusInput").checked) {
-    return true;
-  } else {
-    return false;
+class Book {
+  constructor(title, author, pageCount, readStatus) {
+    this.author = author;
+    this.title = title;
+    this.pageCount = pageCount;
+    this.readStatus = readStatus;
   }
 }
 
-$submit.addEventListener('click', () => {
-  event.preventDefault();
-  addBookToLibrary();
-  displayLibrary();
-})
-
-
-
-function displayLibrary() {
-
-  let libraryElement = document.querySelector("#library");
-  libraryElement.innerHTML = '';
-  for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i];
-    let bookElement = document.createElement("div");
-    bookElement.innerHTML = `
-    <div class = "book-card" id=${i}>
-      <h3 class = "title">${book.title}</h3>
-      
-      <br />
-      <h5 class = "author">by ${book.author}</h5><br />
-      <h5 class = "page-count">${book.pageCount} pages</h5><br />
-      <h5 class = "read-status">Read status: ${book.readStatus}</h5><br />
-      <button type = "button" class="changeReadStatus" onclick="changeReadStatus(${i})">Change read status</button><br />
-      <button type = "button" class="delete" onclick="deleteBook(${i})">Delete!</button>
-    </div> `;
-
-    libraryElement.appendChild(bookElement);
+function addBookToLibrary () {
+    let title = document.querySelector("#titleInput").value;
+    let author = document.querySelector("#authorInput").value;
+    let pageCount = document.querySelector("#pageCountInput").value;
+    let readStatus = checkReadStatus();
+    let newBook = new Book(title, author, pageCount, readStatus);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
   }
-}
 
-function deleteBook(i) {
-  myLibrary.splice(i, 1);
-  displayLibrary();
-}
-
-function changeReadStatus(i) {
-  if (myLibrary[i].readStatus == true) {
-    myLibrary[i].readStatus = false;
-  } else {
-    myLibrary[i].readStatus = true;
+  function deleteBook(i){
+    myLibrary.splice(i, 1);
+    displayLibrary();
   }
-  displayLibrary();
-}
 
+  function checkReadStatus() {
+    if (document.querySelector("#readStatusInput").checked) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function displayLibrary() {
+
+    let libraryElement = document.querySelector("#library");
+    libraryElement.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++) {
+      let book = myLibrary[i];
+      let bookElement = document.createElement("div");
+      bookElement.innerHTML = `
+      <div class = "book-card" id=${i}>
+        <h3 class = "title">${book.title}</h3>
+        
+        <br />
+        <h5 class = "author">by ${book.author}</h5><br />
+        <h5 class = "page-count">${book.pageCount} pages</h5><br />
+        <h5 class = "read-status">Read status: ${book.readStatus}</h5><br />
+        <button type = "button" class="changeReadStatus" onclick="changeReadStatus(${i})">Change read status</button><br />
+        <button type = "button" class="delete" onclick="deleteBook(${i})">Delete!</button>
+      </div> `;
+
+      libraryElement.appendChild(bookElement);
+    }
+  }
+
+  function changeReadStatus(i) {
+    if (myLibrary[i].readStatus == true) {
+      myLibrary[i].readStatus = false;
+    } else {
+      myLibrary[i].readStatus = true;
+    }
+    displayLibrary();
+  }
 
 displayLibrary();
+
+
+
+
+
+
+
+// function Book(title, author, pageCount, readStatus) {
+//   // the constructor...
+//   this.author = author;
+//   this.title = title;
+//   this.pageCount = pageCount;
+//   this.readStatus = readStatus;
+// }
+
+
+
+// const addBookToLibrary = () => {
+//   // do stuff here
+//   let title = document.querySelector("#titleInput").value;
+//   let author = document.querySelector("#authorInput").value;
+//   let pageCount = document.querySelector("#pageCountInput").value;
+//   let readStatus = checkReadStatus();
+//   let newBook = new Book(title, author, pageCount, readStatus);
+//   myLibrary.push(newBook);
+//   console.log(myLibrary);
+// }
+
+
+
+// function deleteBook(i) {
+//   myLibrary.splice(i, 1);
+//   displayLibrary();
+// }
+
+
+
+// function checkReadStatus() {
+//   if (document.querySelector("#readStatusInput").checked) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+
+
+// function displayLibrary() {
+
+//   let libraryElement = document.querySelector("#library");
+//   libraryElement.innerHTML = '';
+//   for (let i = 0; i < myLibrary.length; i++) {
+//     let book = myLibrary[i];
+//     let bookElement = document.createElement("div");
+//     bookElement.innerHTML = `
+//     <div class = "book-card" id=${i}>
+//       <h3 class = "title">${book.title}</h3>
+
+//       <br />
+//       <h5 class = "author">by ${book.author}</h5><br />
+//       <h5 class = "page-count">${book.pageCount} pages</h5><br />
+//       <h5 class = "read-status">Read status: ${book.readStatus}</h5><br />
+//       <button type = "button" class="changeReadStatus" onclick="changeReadStatus(${i})">Change read status</button><br />
+//       <button type = "button" class="delete" onclick="deleteBook(${i})">Delete!</button>
+//     </div> `;
+
+//     libraryElement.appendChild(bookElement);
+//   }
+// }
+
+
+// function changeReadStatus(i) {
+//   if (myLibrary[i].readStatus == true) {
+//     myLibrary[i].readStatus = false;
+//   } else {
+//     myLibrary[i].readStatus = true;
+//   }
+//   displayLibrary();
+// }
+
+
